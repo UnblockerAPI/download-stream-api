6b7d405c0353c87203838fa6170218fe592c4863
Marise Hayashi <vyatheslav2707@mail.ru>
2018-10-14 23:09:47 +0300

    Utils updates

--- modules/utils.js
+++ modules/utils.js
@@ -4,38 +4,41 @@ const request = require('request');
 module.exports = {
     checkAvailability(url) {
         return new Promise(resolve => {
-            request({
-                method: 'HEAD',
+            let r = request({
+                method: 'GET',
                 uri: url,
                 headers: {
                     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36'
                 }
-            },
-            (err, httpResponse, body) => {
-                if (err || String(httpResponse.statusCode).match(/^(4|5)\d{2}$/)) {
+            });
+
+            r.on('response', response => {
+                r.abort();
+
+                if (String(response.statusCode).match(/^(4|5)\d{2}$/)) {
                     return resolve({ isOk: false, headers: null });
                 }
 
-                return resolve({ isOk: true, headers: httpResponse.headers });
+                return resolve({ isOk: true, headers: response.headers });
+            });
+
+            r.on('error', err => {
+                r.abort();
+                return resolve({ isOk: false, headers: null });
             });
         });
     },
 
     streamFile({ writableStream, file }) {
-        if (typeof file === 'object' && typeof file.pipe === 'function') {
-            return file.once('end', () => {
-                file.destroy();
-                fs.unlink(file, err => {});
-            }).pipe(writableStream);
-
-        } else if (typeof file === 'string') {
-            return request({
-                method: 'GET',
-                uri: file,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36'
-                }
-            }).pipe(writableStream);
-        }
+        return request({
+            method: 'GET',
+            uri: file,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36'
+            }
+        }).on('response', response => {
+            delete response.headers['content-disposition'];
+            delete response.headers['content-type'];
+        }).pipe(writableStream);
     }
 }
