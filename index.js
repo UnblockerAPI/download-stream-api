ff28a1086939a045b372769e2bac802d48b902de
Marise Hayashi <vyatheslav2707@mail.ru>
2019-02-21 12:23:00 +0300

    Update env + bump deps & formatting

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-const express = require('express');
-const compression = require('compression');
-const helmet = require('helmet');
-const fs = require('fs');
-const mime = require('mime-types');
+const express = require("express");
+const compression = require("compression");
+const helmet = require("helmet");
+const fs = require("fs");
+const mime = require("mime-types");
 
-
-let isProduction = process.env.NODE_ENV === 'production';
-let PORT = isProduction ? '/tmp/nginx.socket' : 8080;
+let isProduction = process.env.NODE_ENV === "production";
+let PORT = isProduction ? "/tmp/nginx.socket" : 8080;
 let callbackFn = () => {
-    if (isProduction) {
-        fs.closeSync(fs.openSync('/tmp/app-initialized', 'w'));
-    }
+  if (isProduction) {
+    fs.closeSync(fs.openSync("/tmp/app-initialized", "w"));
+  }
 
-    console.log(`Listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 };
 
-
-const utils = require('./modules/utils');
-
+const utils = require("./modules/utils");
 
 const app = express();
 app.enable("trust proxy", 1);
 app.use(helmet());
 app.use(compression());
 
-app.get('/', async (req, res) => {
-    try {
-        let targetUrl = new URL(Buffer.from(req.query.url, 'base64').toString('ascii'));
+app.get("/", async (req, res) => {
+  try {
+    let targetUrl = new URL(
+      Buffer.from(req.query.url, "base64").toString("ascii")
+    );
 
-        let { isOk, headers } = await utils.checkAvailability(targetUrl.href);
-        if (!isOk) {
-            return res.status(400).json({ success: false, reason: "Non200StatusCode" });
-        }
-
-        let contentType = headers["content-type"] || 'application/octet-stream';
-        let filename = 'download.' + ( mime.extension(contentType) || 'bin' );
-
-        res.status(200);
-        res.set({
-            'Content-Type': contentType,
-            'Content-Disposition': `attachment; filename="${filename}"`
-        });
-
-        return utils.streamFile({ writableStream: res, file: targetUrl.href });
-
-    } catch (e) {
-        return res.status(400).json({ success: false, reason: "InvalidURL" });
+    let { isOk, headers } = await utils.checkAvailability(targetUrl.href);
+    if (!isOk) {
+      return res
+        .status(400)
+        .json({ success: false, reason: "Non200StatusCode" });
     }
+
+    let contentType = headers["content-type"] || "application/octet-stream";
+    let filename = "download." + (mime.extension(contentType) || "bin");
+
+    res.status(200);
+    res.set({
+      "Content-Type": contentType,
+      "Content-Disposition": `attachment; filename="${filename}"`
+    });
+
+    return utils.streamFile({ writableStream: res, file: targetUrl.href });
+  } catch (e) {
+    return res.status(400).json({ success: false, reason: "InvalidURL" });
+  }
 });
 
 app.listen(PORT, callbackFn);
--- modules/utils.js
+++ modules/utils.js
@@ -1,44 +1,47 @@
-const request = require('request');
-
+const request = require("request");
 
 module.exports = {
-    checkAvailability(url) {
-        return new Promise(resolve => {
-            let r = request({
-                method: 'GET',
-                uri: url,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36'
-                }
-            });
+  checkAvailability(url) {
+    return new Promise(resolve => {
+      let r = request({
+        method: "GET",
+        uri: url,
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36"
+        }
+      });
 
-            r.on('response', response => {
-                r.abort();
+      r.on("response", response => {
+        r.abort();
 
-                if (String(response.statusCode).match(/^(4|5)\d{2}$/)) {
-                    return resolve({ isOk: false, headers: null });
-                }
+        if (String(response.statusCode).match(/^(4|5)\d{2}$/)) {
+          return resolve({ isOk: false, headers: null });
+        }
 
-                return resolve({ isOk: true, headers: response.headers });
-            });
+        return resolve({ isOk: true, headers: response.headers });
+      });
 
-            r.on('error', err => {
-                r.abort();
-                return resolve({ isOk: false, headers: null });
-            });
-        });
-    },
+      r.on("error", err => {
+        r.abort();
+        return resolve({ isOk: false, headers: null });
+      });
+    });
+  },
 
-    streamFile({ writableStream, file }) {
-        return request({
-            method: 'GET',
-            uri: file,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36'
-            }
-        }).on('response', response => {
-            delete response.headers['content-disposition'];
-            delete response.headers['content-type'];
-        }).pipe(writableStream);
-    }
-}
+  streamFile({ writableStream, file }) {
+    return request({
+      method: "GET",
+      uri: file,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3563.0 Safari/537.36"
+      }
+    })
+      .on("response", response => {
+        delete response.headers["content-disposition"];
+        delete response.headers["content-type"];
+      })
+      .pipe(writableStream);
+  }
+};
